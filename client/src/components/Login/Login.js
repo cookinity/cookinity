de0194a21ca2b486d8ab90f95b757ef0312ff3fe
chrisdeu <christina.deutsch@tum.de>
2021-06-02 09:32:30 +0200

    Added bootstrap to Register and Login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Link, withRouter, Redirect } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import { withRouter, Redirect } from 'react-router-dom';
 
 import { useFormik } from 'formik';
 
@@ -9,7 +12,6 @@ import _ from 'lodash';
 
 import { loginUserWithEmail } from '../../store/features/authentication/authActions';
 import { loginSchema } from './validation';
-import './styles.scss';
 
 const Login = ({ auth, history, loginUserWithEmail }) => {
   const formik = useFormik({
@@ -26,61 +28,68 @@ const Login = ({ auth, history, loginUserWithEmail }) => {
   if (auth.isAuthenticated) return <Redirect to="/" />;
 
   return (
-    <div className="login">
-      <div className="container">
-        <h1>Log in page</h1>
+    <div className="w-25 p-3 mb-1 bg-secondary text-light">
+      <Form onSubmit={formik.handleSubmit} noValidate>
         <p>
-          back to{' '}
-          <Link className="bold" to="/">
-            Home page
-          </Link>
+          Back to <a href="/">Home page</a>
         </p>
-        <form onSubmit={formik.handleSubmit}>
-          <h2>Login with email address</h2>
-          <div>
-            <input
-              placeholder="Email address"
+
+        <h3>Login</h3>
+        <div>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email address"
               name="email"
-              className="text"
-              type="text"
+              required
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.email}
             />
-            {formik.touched.email && formik.errors.email ? (
-              <p className="error">{formik.errors.email}</p>
-            ) : null}
-            <input
-              placeholder="Password"
-              name="password"
+          </Form.Group>
+
+          {formik.touched.email && formik.errors.email ? (
+            <p className="error">{formik.errors.email}</p>
+          ) : null}
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
               type="password"
-              className="text"
+              placeholder="Enter password"
+              name="password"
+              required
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.password}
             />
-            {formik.touched.password && formik.errors.password ? (
-              <p className="error">{formik.errors.password}</p>
-            ) : null}
-          </div>
-          {auth.error && <p className="error">{auth.error}</p>}
-          <div>
-            <button
-              className="btn submit"
-              disabled={auth.isLoading || !formik.isValid}
-              type="submit"
-            >
-              Log in now
-            </button>
-          </div>
-          <div>
-            Don't have an account?{' '}
-            <Link className="bold" to="/register">
-              Register
-            </Link>
-          </div>
-        </form>
-      </div>
+          </Form.Group>
+
+          {formik.touched.password && formik.errors.password ? (
+            <p className="error">{formik.errors.password}</p>
+          ) : null}
+        </div>
+
+        {auth.error && <p className="error">{auth.error}</p>}
+
+        <Button
+          className="btn submit"
+          variant="primary"
+          type="submit"
+          disabled={auth.isLoading || !formik.isValid}
+        >
+          Log in now
+        </Button>
+
+        <p>
+          Don't have an account? <a href="/register">Register</a>
+        </p>
+
+        <p className="forgot-password">
+          Forgot <a href="#">password</a>?
+        </p>
+      </Form>
     </div>
   );
 };
