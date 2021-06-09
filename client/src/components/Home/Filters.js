f6e3f690764a314bb9016bf7dd540318bc963e37
chrisdeu <christina.deutsch@tum.de>
2021-06-09 15:08:44 +0200

    Added search within dropdown

--- client/src/components/Home/Filters.js
+++ client/src/components/Home/Filters.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const Filters = () => {
+
+    const toggleFunction = () => {
+        document.getElementById("dropdownMenu").classList.toggle("show");
+    }
+      
+    const showContent = () => {
+        var input = document.getElementById("searchInput");
+        var filter = input.value;
+        var div = document.getElementById("dropdownMenu");
+        var a = div.getElementsByTagName("a");
+        var i;
+        var txtValue;
+        for (i = 0; i < a.length; i++) {
+            txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+            } else {
+                a[i].style.display = "none";
+    }
+  }
+}
+
     return (
-    <div className="dropdown">
-        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Category
-        </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item" href="#">European</a>
-            <a className="dropdown-item" href="#">Asian</a>
-            <a className="dropdown-item" href="#">Indian</a>
-            <a className="dropdown-item" href="#">German</a>
-            <a className="dropdown-item" href="#">Chinese</a>
-        </div>
-    </div>
+            <div className="dropdown">
+                <button onClick = {showContent} className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Category
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" id="dropdownMenu">
+                    <input onKeyUp= {toggleFunction} type="text" placeholder="Search.." id="searchInput" ></input>
+                    <a className="dropdown-item" href="#european">European</a>
+                    <a className="dropdown-item" href="#asian">Asian</a>
+                    <a className="dropdown-item" href="#indian">Indian</a>
+                    <a className="dropdown-item" href="#german">German</a>
+                    <a className="dropdown-item" href="#chinese">Chinese</a>
+                </div>
+            </div>
     );
 }
 
