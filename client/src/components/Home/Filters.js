e409c75eb61e5c50a48a6cf3ac4eea21b47470cb
chrisdeu <christina.deutsch@tum.de>
2021-06-13 18:08:39 +0200

    Added delete class by id method

--- client/src/components/Home/Filters.js
+++ client/src/components/Home/Filters.js
@@ -3,45 +3,19 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 
 const Filters = () => {
 
-    const toggleFunction = () => {
-        document.getElementById("dropdownMenu").classList.toggle("show");
-    }
-      
-    const showContent = () => {
-        var input = document.getElementById("searchInput");
-        var filter = input.value;
-        var div = document.getElementById("dropdownMenu");
-        var a = div.getElementsByTagName("a");
-        var i;
-        var txtValue;
-        for (i = 0; i < a.length; i++) {
-            txtValue = a[i].textContent || a[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                a[i].style.display = "";
-            } else {
-                a[i].style.display = "none";
-    }
-  }
-}
-
-    return (
-            <div className="dropdown">
-                <button onClick = {showContent} className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Category
-                </button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" id="dropdownMenu">
-                    <input onKeyUp= {toggleFunction} type="text" placeholder="Search.." id="searchInput" ></input>
-                    <a className="dropdown-item" href="#european">European</a>
-                    <a className="dropdown-item" href="#asian">Asian</a>
-                    <a className="dropdown-item" href="#indian">Indian</a>
-                    <a className="dropdown-item" href="#german">German</a>
-                    <a className="dropdown-item" href="#chinese">Chinese</a>
-                </div>
+      return (           
+            <div>
+            <select className="form-control selectpicker" id="select-country" data-live-search="true">
+                <option value="" disabled selected>Choose category</option>
+                <option value="1">European</option>
+                <option value="2">Asian</option>
+                <option value="3">Indian</option>
+                <option value="3">German</option>
+                <option value="3">Chinese</option>
+            </select>
             </div>
     );
 }
 
 export default Filters
-
-
 
