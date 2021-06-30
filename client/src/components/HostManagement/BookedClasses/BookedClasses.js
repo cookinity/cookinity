36c5f8bc6ba7abfe1a752a772f9df4099c702153
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-06-30 21:07:23 +0200

    Fixed Joes Bug

--- client/src/components/HostManagement/BookedClasses/BookedClasses.js
+++ client/src/components/HostManagement/BookedClasses/BookedClasses.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Layout from 'components/Layout/Layout'
+import Layout from 'components/Layout/Layout';
 import { compose } from 'redux';
 import requireAuth from 'higherOrderComponents/requireAuth';
 import Loader from 'components/Shared/Loader/Loader';
@@ -9,97 +9,92 @@ import { useSelector } from 'react-redux';
 import { Alert, Col, Row } from 'react-bootstrap';
 import { BookedClassesTable } from './BookedClassesTable';
 
-
 const BookedClasses = () => {
-    const [futureBookings, setFutureBookings] = useState([]);
-    const [pastBookings, setPastBookings] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    const auth = useSelector((state) => state.auth);
-    
-    useEffect(() => {
-        fetchBookings();
-      }, []);
+  const [futureBookings, setFutureBookings] = useState([]);
+  const [pastBookings, setPastBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const auth = useSelector((state) => state.auth);
 
-    const fetchBookings = async () => {
-        setIsError(false);
-        setIsLoading(true);
-        try {
-          // adding the necessary security header
-            const token = auth.token;
-            const config = {
-                headers: {
-                'Content-type': 'application/json',
-                },
-            };
-            if (token) {
-                config.headers['x-auth-token'] = token;
-            }  
-          // load all bookings for which the currently logged in user is host
-          const result = await axios.get('/api/bookings', config);
-          const unformattedBookings = result.data.bookings;
-          const classesBookedFuture = [];
-          const classesBookedPast = [];
-          const today = dayjs(new Date());
-          unformattedBookings.forEach((b) => {
-            const date = dayjs(b.bookedTimeSlot);
-            if (date.isBefore(today)) {
-                classesBookedPast.push(b)
-            } else {
-                classesBookedFuture.push(b)
-                }
-            });
+  useEffect(() => {
+    fetchBookings();
+  }, []);
 
-        setFutureBookings(classesBookedFuture);
-        setPastBookings(classesBookedPast);
-        } catch (err) {
-          setIsError(true);
-          setErrorMessage(err?.response?.data.message || err.message);
-        }
-        setIsLoading(false);
+  const fetchBookings = async () => {
+    setIsError(false);
+    setIsLoading(true);
+    try {
+      // adding the necessary security header
+      const token = auth.token;
+      const config = {
+        headers: {
+          'Content-type': 'application/json',
+        },
       };
+      if (token) {
+        config.headers['x-auth-token'] = token;
+      }
+      // load all bookings for which the currently logged in user is host
+      const result = await axios.get('/api/bookings', config);
+      const unformattedBookings = result.data.bookings;
+      const classesBookedFuture = [];
+      const classesBookedPast = [];
+      const today = dayjs(new Date());
+      unformattedBookings.forEach((b) => {
+        const date = dayjs(b.bookedTimeSlot.date);
+        if (date.isBefore(today)) {
+          classesBookedPast.push(b);
+        } else {
+          classesBookedFuture.push(b);
+        }
+      });
 
-    if (isLoading) {
-        return (
-          <Layout>
-            <Loader></Loader>
-          </Layout>
-        );
-    } else {
+      setFutureBookings(classesBookedFuture);
+      setPastBookings(classesBookedPast);
+    } catch (err) {
+      setIsError(true);
+      setErrorMessage(err?.response?.data.message || err.message);
+    }
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
     return (
-        <Layout>
-            <Row>
-                <Col>
-                    <div>
-                    {isError && (
-                        <Alert
-                        variant="danger"
-                        onClose={() => {
-                            setIsError(false);
-                            setErrorMessage('');
-                        }}
-                        dismissible
-                        >
-                        {' '}
-                        {errorMessage}
-                        </Alert>
-                    )}
-                    <h1 className="text-center">Future Bookings</h1>
-                    <BookedClassesTable
-                        bookings={futureBookings}
-                    ></BookedClassesTable>
-                    <hr></hr>
-                    <h1 className="text-center">Past Bookings</h1>
-                    <BookedClassesTable
-                        bookings={pastBookings}
-                    ></BookedClassesTable>
-                    </div>
-                </Col>
-            </Row>
-        </Layout>
-        );
-    }   
-}
+      <Layout>
+        <Loader></Loader>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <Row>
+          <Col>
+            <div>
+              {isError && (
+                <Alert
+                  variant="danger"
+                  onClose={() => {
+                    setIsError(false);
+                    setErrorMessage('');
+                  }}
+                  dismissible
+                >
+                  {' '}
+                  {errorMessage}
+                </Alert>
+              )}
+              <h1 className="text-center">Future Bookings</h1>
+              <BookedClassesTable bookings={futureBookings}></BookedClassesTable>
+              <hr></hr>
+              <h1 className="text-center">Past Bookings</h1>
+              <BookedClassesTable bookings={pastBookings}></BookedClassesTable>
+            </div>
+          </Col>
+        </Row>
+      </Layout>
+    );
+  }
+};
 
 export default compose(requireAuth)(BookedClasses);
