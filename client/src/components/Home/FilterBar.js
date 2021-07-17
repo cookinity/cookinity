972bb3bc4735cfc8caa9923becb923eeb9b623bd
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-07-18 00:07:33 +0200

    Updated design of filter bar

--- client/src/components/Home/FilterBar.js
+++ client/src/components/Home/FilterBar.js
@@ -14,71 +14,88 @@ const FilterBar = ({
   fetchClasses,
 }) => {
   return (
-    <Form>
-      {' '}
-      <Form.Row>
-        <Form.Group as={Col} xs={12} md={{ span: 3, offset: 1 }}>
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text>City</InputGroup.Text>
-            </InputGroup.Prepend>
-            <Form.Control as="select" custom onChange={handleFilterCity}>
-              <option>All Cities</option>
-              {CITY_CATEGORIES.map((c) => (
-                <option>{c}</option>
-              ))}
-            </Form.Control>
-          </InputGroup>
-        </Form.Group>
-        <Form.Group as={Col} xs={12} md="3" controlId="validationCustom02">
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text>Category</InputGroup.Text>
-            </InputGroup.Prepend>
-            <Form.Control as="select" custom onChange={handleFilterCategory}>
-              <option>All Categories</option>
-              {CLASS_CATEGORIES.map((c) => (
-                <option>{c}</option>
-              ))}
-            </Form.Control>
-          </InputGroup>
-        </Form.Group>
-        <Form.Group as={Col} xs={12} md="3" controlId="validationCustom02">
-          <DatePicker
-            value={startDate}
-            onChange={handleFilterDate}
-            type="custom"
-            render={(stringDate, openCalendar) => {
-              return (
-                <InputGroup>
-                  <InputGroup.Prepend>
-                    <InputGroup.Text>Date</InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control
-                    placeholder="All Dates"
-                    onClick={openCalendar}
-                    value={stringDate}
-                  ></Form.Control>
-                </InputGroup>
-              );
-            }}
-            plugins={[
-              <Toolbar
-                position="bottom"
-                names={{
-                  today: 'select today',
-                  deselect: 'select none',
-                  close: 'close',
-                }}
-              />,
-            ]}
-          />
-        </Form.Group>
-        <Col xs={12} md={1} className="text-left">
-          <Button onClick={fetchClasses}> Search</Button>
-        </Col>
-      </Form.Row>
-    </Form>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card border-light">
+              <div className="card-body">
+                <Form>
+                  <Form.Row>
+                    <Form.Group as={Col} xs={12} md={{ span: 3, offset: 1 }} className="my-auto">
+                      <InputGroup>
+                        <InputGroup.Prepend>
+                          <InputGroup.Text>
+                            <FontAwesomeIcon icon="map-marker" />
+                          </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control as="select" custom onChange={handleFilterCity}>
+                          <option>All Cities</option>
+                          {CITY_CATEGORIES.map((c) => (
+                            <option>{c}</option>
+                          ))}
+                        </Form.Control>
+                      </InputGroup>
+                    </Form.Group>
+                    <Form.Group as={Col} xs={12} md="3" className="my-auto">
+                      <InputGroup>
+                        <InputGroup.Prepend>
+                          <InputGroup.Text>
+                            <FontAwesomeIcon icon="hamburger" />
+                          </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control as="select" custom onChange={handleFilterCategory}>
+                          <option>All Categories</option>
+                          {CLASS_CATEGORIES.map((c) => (
+                            <option>{c}</option>
+                          ))}
+                        </Form.Control>
+                      </InputGroup>
+                    </Form.Group>
+                    <Form.Group as={Col} xs={12} md="3" className="my-auto">
+                      <DatePicker
+                        value={startDate}
+                        onChange={handleFilterDate}
+                        type="custom"
+                        render={(stringDate, openCalendar) => {
+                          return (
+                            <InputGroup>
+                              <InputGroup.Prepend>
+                                <InputGroup.Text>
+                                  <FontAwesomeIcon icon="calendar" />
+                                </InputGroup.Text>
+                              </InputGroup.Prepend>
+                              <Form.Control
+                                placeholder="All Dates"
+                                onClick={openCalendar}
+                                value={stringDate}
+                              ></Form.Control>
+                            </InputGroup>
+                          );
+                        }}
+                        plugins={[
+                          <Toolbar
+                            position="bottom"
+                            names={{
+                              today: 'select today',
+                              deselect: 'select none',
+                              close: 'close',
+                            }}
+                          />,
+                        ]}
+                      />
+                    </Form.Group>
+                    <Col xs={12} md={1} className="text-left">
+                      <Button onClick={fetchClasses}> Search</Button>
+                    </Col>
+                  </Form.Row>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
