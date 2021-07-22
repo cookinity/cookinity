d7267f370ce25b38b70b8a94a3b1b5a404069a6f
chrisdeu <christina.deutsch@tum.de>
2021-07-22 23:44:59 +0200

    Fixed pagination location

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -277,26 +277,26 @@ export const Home = () => {
 
                   {classCards}
                 </Row>
-                <Row>
-                  <Col xs={12} className="text-center mt-2 mb-4">
-                    <div className="btn-group" role="group">
-                      <Button onClick={previousPage} disabled={skip === 0 ? true : false}>
-                        Previous
-                      </Button>
-                      <Button
-                        onClick={nextPage}
-                        disabled={skip + filteredClasses.length >= numberOfEntries ? true : false}
-                      >
-                        Next
-                      </Button>
-                    </div>
-                  </Col>
-                </Row>
               </Container>
             </Col>
             <Col xs={12} lg={12} xl={4}>
               <div style={{ display: filteredClasses.length > 0 ? 'block' : 'none' }}>
                 <ClassesMap classes={filteredClasses}></ClassesMap>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} className="text-center mt-2 mb-4">
+              <div className="btn-group" role="group">
+                <Button onClick={previousPage} disabled={skip === 0 ? true : false}>
+                  Previous
+                </Button>
+                <Button
+                  onClick={nextPage}
+                  disabled={skip + filteredClasses.length >= numberOfEntries ? true : false}
+                >
+                  Next
+                </Button>
               </div>
             </Col>
           </Row>
