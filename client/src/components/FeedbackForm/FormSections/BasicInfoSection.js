ae1c6ffa10a66e805ec7cc486b9af2bb8d40e9a0
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-07-25 09:38:14 +0200

    Added margin to feedback form

--- client/src/components/FeedbackForm/FormSections/BasicInfoSection.js
+++ client/src/components/FeedbackForm/FormSections/BasicInfoSection.js
@@ -2,56 +2,68 @@ import Rating from 'components/Home/Rating';
 import React, { useState } from 'react';
 import { Col, Form, InputGroup } from 'react-bootstrap';
 
-const BasicInfoSection = ({ formik,
-  setRatingOverallRatingStars, overallRatingStars,
-  setRatingHostStars, hostRatingStars,
-  setRatingTasteStars, tasteRatingStars,
-  setRatingLocationStars, locationRatingStars,
-  setRatingVtmrStars, vtmrRatingStars,
-  setRatingExperienceStars, experienceRatingStars
+const BasicInfoSection = ({
+  formik,
+  setRatingOverallRatingStars,
+  overallRatingStars,
+  setRatingHostStars,
+  hostRatingStars,
+  setRatingTasteStars,
+  tasteRatingStars,
+  setRatingLocationStars,
+  locationRatingStars,
+  setRatingVtmrStars,
+  vtmrRatingStars,
+  setRatingExperienceStars,
+  experienceRatingStars,
 }) => {
-
   return (
     <>
       <div className="row">
-        <h3 className='col text-center'>Overall Rating</h3>
-        <h3 className='col text-center'>Host Rating</h3>
+        <h3 className="col text-center">Overall Rating</h3>
+        <h3 className="col text-center">Host Rating</h3>
       </div>
 
       <div className="row">
-        <div className='col text-center'>
-          <Rating rating={overallRatingStars} onRating={rate => setRatingOverallRatingStars(rate)} />
+        <div className="col text-center">
+          <Rating
+            rating={overallRatingStars}
+            onRating={(rate) => setRatingOverallRatingStars(rate)}
+          />
         </div>
-        <div className='col text-center'>
-          <Rating rating={hostRatingStars} onRating={rate => setRatingHostStars(rate)} />
-        </div>
-      </div>
-
-      <div className="row">
-        <h3 className='col text-center'>Taste Rating</h3>
-        <h3 className='col text-center'>Location Rating</h3>
-      </div>
-
-      <div className='row'>
-        <div className='col text-center'>
-          <Rating rating={tasteRatingStars} onRating={rate => setRatingTasteStars(rate)} />
-        </div>
-        <div className='col text-center'>
-          <Rating rating={locationRatingStars} onRating={rate => setRatingLocationStars(rate)} />
+        <div className="col text-center">
+          <Rating rating={hostRatingStars} onRating={(rate) => setRatingHostStars(rate)} />
         </div>
       </div>
 
       <div className="row">
-        <h3 className='col text-center'>Value to money ratio</h3>
-        <h3 className='col text-center'>Experience Rating</h3>
+        <h3 className="col text-center">Taste Rating</h3>
+        <h3 className="col text-center">Location Rating</h3>
       </div>
 
-      <div className='row'>
-        <div className='col text-center'>
-          <Rating rating={vtmrRatingStars} onRating={rate => setRatingVtmrStars(rate)} />
+      <div className="row">
+        <div className="col text-center">
+          <Rating rating={tasteRatingStars} onRating={(rate) => setRatingTasteStars(rate)} />
         </div>
-        <div className='col text-center'>
-          <Rating rating={experienceRatingStars} onRating={rate => setRatingExperienceStars(rate)} />
+        <div className="col text-center">
+          <Rating rating={locationRatingStars} onRating={(rate) => setRatingLocationStars(rate)} />
+        </div>
+      </div>
+
+      <div className="row">
+        <h3 className="col text-center">Value to money ratio</h3>
+        <h3 className="col text-center">Experience Rating</h3>
+      </div>
+
+      <div className="row">
+        <div className="col text-center">
+          <Rating rating={vtmrRatingStars} onRating={(rate) => setRatingVtmrStars(rate)} />
+        </div>
+        <div className="col text-center">
+          <Rating
+            rating={experienceRatingStars}
+            onRating={(rate) => setRatingExperienceStars(rate)}
+          />
         </div>
       </div>
 
@@ -64,7 +76,9 @@ const BasicInfoSection = ({ formik,
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.overallRating}
-          className={formik.touched.overallRating && formik.errors.overallRating ? 'form-error' : null}
+          className={
+            formik.touched.overallRating && formik.errors.overallRating ? 'form-error mt-4' : 'mt-4'
+          }
         />
         {formik.touched.overallRating && formik.errors.overallRating ? (
           <div className="form-error-message">{formik.errors.overallRating}</div>
